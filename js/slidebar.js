bca4e1d750e6e9696865b3c2557593a901d7403d
acima <acimakd@gmail.com>
2017-10-24 14:57:11 +0200

    Ajout de la slidebarmenu droite,js ouverture et fermeture

--- js/slidebar.js
+++ js/slidebar.js
@@ -1,3 +1,9 @@
-(function () {
+document.getElementById('slide').onclick = function () {
+	if (document.getElementById('slidebar').style.display == 'none');
+		document.getElementById('slidebar').style.display = 'table';
+}
 
-}) ()
+document.getElementById('closeSlidebar').onclick = function () {
+	if (document.getElementById('slidebar').style.display == 'table');
+	document.getElementById('slidebar').style.display = 'none';
+}
